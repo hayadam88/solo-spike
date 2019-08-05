5a0d82f38c394247acb84c5be5a3f27f14b3285d
Adam Hay <heyadam66@gmail.com>
2019-08-04 19:33:40 -0500

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi there!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
